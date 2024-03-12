3ac12413014f3af8203b57d4277af89e2c279490
tanaliga100 <43674051+tanaliga100@users.noreply.github.com>
2024-03-12 14:22:28 +0800

    add-serializers

---
+++ backend/core/admin.py
@@ -0,0 +1,10 @@
+
+# Register your models here.
+
+from django.contrib import admin
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description", "email")
--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -13,9 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+
+from django.urls import path
+from django.contrib import admin
+from core import views as core_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path("admin/", admin.site.urls),
+    path("contact/", core_views.ContactAPIView.as_view()),
 ]
